4822e967f3b229b5f2d2b8391621a62a0cccd263
seongukkwon1234 <seongukkwon38@gmail.com>
2024-09-04 13:42:59 +0900

    [Feat]이동 소스 틀 제작

--- Assets/scriot/HelloWorld.cs
+++ Assets/scriot/HelloWorld.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
-{
-    // Start is called before the first frame update
+{  
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
