561b3c3c9297e29d17cea8344bb9e8a1c6629ace
seongukkwon1234 <seongukkwon38@gmail.com>
2024-09-04 13:47:32 +0900

    [Feat] 이동 변수 생성

--- Assets/scriot/HelloWorld.cs
+++ Assets/scriot/HelloWorld.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
-{  
+{
+    public Vector3 move;
+
     void Start()
     {
         Debug.Log("Hello World");
